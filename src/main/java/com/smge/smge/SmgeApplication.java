ab582909b7edfb9febecef4874c143880c8ddd7d
luizing <luizeduardo2099@gmail.com>
2026-09-22 19:32:39 -0300

    init: Cria projeto Spring com dependencias

---
+++ src/main/java/com/smge/smge/SmgeApplication.java
@@ -0,0 +1,13 @@
+package com.smge.smge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smge/smge/SmgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smge.smge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
